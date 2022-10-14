fb85bf4f90cd1803a9e051478436173f6d8d3f64
Luca Andrey <lucaandrey5@hotmail.com>
2022-10-14 14:16:24 -0300

    api pokedex em react

---
+++ src/pages/Home.jsx
@@ -0,0 +1,58 @@
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import PokemonCard from "../components/PokemonCard";
+
+const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    let endpoints = [];
+    for (let i = 1; i < 200; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    }
+    console.log(endpoints);
+    axios
+      .all(endpoints.map((endpoint) => axios.get(endpoint)))
+      .then((resp) => setPokemons(resp));
+  };
+
+  const pokemonFilter = (name) => {
+    let filteredPokemons = [];
+    if (name === "") {
+      getPokemons();
+    }
+    for (let i in pokemons) {
+      if (pokemons[i].data.name.includes(name)) {
+        filteredPokemons.push(pokemons[i]);
+      }
+    }
+    setPokemons(filteredPokemons);
+  };
+
+  return (
+    <div>
+      <NavBar pokemonFilter={pokemonFilter} />
+      <Container maxWidth="false">
+        <Grid container spacing={3}>
+          {pokemons.map((pokemon, key) => (
+            <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
+              <PokemonCard
+                name={pokemon.data.name}
+                image={pokemon.data.sprites.front_default}
+                types={pokemon.data.types}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
